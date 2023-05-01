e0cb5e2efef1ff2975a2b6dac15fed4480f4d0b7
Thiago Marcos da Silva <thiagobossun06@gmail.com>
2023-05-01 14:27:24 -0300

    Adicao de spring boot ao projeto

--- catalogo/src/main/java/com/example/catalogo/Usuario.java
+++ catalogo/src/main/java/com/example/catalogo/Usuario.java
@@ -1,7 +1,22 @@
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private Long id;
+
+    @Column(name = "nome")
     private String nome;
+    
     private Anime anime;
     private Filme filme;
     private Livro livro;
