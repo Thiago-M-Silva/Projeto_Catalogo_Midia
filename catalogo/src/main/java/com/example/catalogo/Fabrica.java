ae78619c19520cfdc25c592cec00ca2684ab3afd
Thiago-M-Silva <thiagobossun06@gmail.com>
2023-08-31 16:57:17 -0300

    Correcoes, integracao com o banco de dados normalizada, exclusao da classe ator e de vestigios da classe midia, reorganização dos atributos de todas as classes

--- catalogo/src/main/java/com/example/catalogo/Fabrica.java
+++
@@ -1,7 +0,0 @@
-//package com.example.catalogo;
-//
-//import com.example.catalogo.Midia.midia;
-//
-//public abstract class Fabrica {
-//    public abstract midia criaMidia(String nome, String autor, String estudio);
-//}
